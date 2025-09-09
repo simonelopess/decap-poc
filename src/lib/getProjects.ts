56dbd91d170951195c9dc2f1b9a0871a0168cdc1
Simone <simone.l.silva@parceiros.nos.pt>
2025-09-09 17:35:05 +0100

    config astro to consumer decap cms

---
+++ src/lib/getProjects.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+export function getProjects(project: string) {
+  const projectsDir = path.join(process.cwd(), `src/content/${project}`);
+  const filenames = fs.readdirSync(projectsDir);
+
+  return filenames.map((filename) => {
+    const filePath = path.join(projectsDir, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const { data, content } = matter(fileContents);
+
+    return {
+      slug: filename.replace(/\.md$/, ''),
+      ...data,
+      content,
+    };
+  });
+}
